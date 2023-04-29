af06eae92c519789e71d62df978416ec01db7d0c
Martin <martin.seal@spicatech.co.uk>
2023-04-30 00:54:24 +0100

    UPDATE to restore project

--- lib/dependency_injection_container.dart
+++ lib/dependency_injection_container.dart
@@ -3,13 +3,14 @@ import 'package:get_it/get_it.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:package_info/package_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:simple_aac/view_models/selected_words_view_model.dart';
 
 import 'services/navigation_service.dart';
 import 'services/shared_preferences_service.dart';
 import 'view_models/create_word/create_word_view_model.dart';
 import 'view_models/file_picker/file_picker_view_model.dart';
 import 'view_models/intro/intro_view_model.dart';
+import 'view_models/selected_words_view_model.dart';
+import 'view_models/theme_view_model.dart';
 import 'view_models/utils/tab_bar_view_model.dart';
 
 final getIt = GetIt.instance;
@@ -17,13 +18,14 @@ final getIt = GetIt.instance;
 Future<void> init() async {
   getIt.registerLazySingletonAsync(SharedPreferences.getInstance);
   getIt.registerLazySingletonAsync(PackageInfo.fromPlatform);
-  getIt.registerLazySingleton(() => SharedPreferencesService());
+  getIt.registerLazySingleton(SharedPreferencesService.new);
   getIt.registerLazySingleton(() => const FlutterSecureStorage());
-  getIt.registerLazySingleton(() => NavigationService());
-  getIt.registerLazySingleton(() => ImagePicker());
-  getIt.registerLazySingleton(() => SelectedWordsViewModel());
-  getIt.registerFactory(() => IntroViewModel());
-  getIt.registerFactory(() => TabBarViewModel());
+  getIt.registerLazySingleton(NavigationService.new);
+  getIt.registerLazySingleton(ImagePicker.new);
+  getIt.registerLazySingleton(SelectedWordsViewModel.new);
+  getIt.registerFactory(() => ThemeViewModel(getIt()));
+  getIt.registerFactory(IntroViewModel.new);
+  getIt.registerFactory(TabBarViewModel.new);
   getIt.registerFactory(() => FilePickerViewModel(getIt()));
-  getIt.registerFactory(() => CreateWordViewModel());
+  getIt.registerFactory(CreateWordViewModel.new);
 }
